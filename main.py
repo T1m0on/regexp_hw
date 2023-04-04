1b273e2c0d3f28b65cb92ef1bf2cd0bb23502073
Timofey Prokhorov <prokhorovtd@yandex.ru>
2023-04-04 11:26:03 +0300

    Updates

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ def create_filtered_list():
     filtered_list = []
     for data in read_csv():
         filtered_names = list(filter(None, re.split(r'\s', ' '.join(data[0:3]))))
-        if data[5]:
-            filtered_names.append(data[5])
-            filtered_list.append(filtered_names)
-        for names in filtered_list:
-            if filtered_names[:2] in names[:2]:
-                filtered_list.remove(names)
+        while len(filtered_names) != 3:
+            filtered_names.append(' ')
+        for i in range(3, 7):
+            filtered_names.append(data[i])
+        filtered_list.append(filtered_names)
+    return filtered_list
 
+
+def merge_same_data():
+    filtered_list = create_filtered_list()
+    for filtered_data in filtered_list:
+        for reversed_data in filtered_list[::-1]:
+            if filtered_data[:2] == reversed_data[:2] and filtered_data != reversed_data:
+                for i in range(7):
+                    if filtered_data[i]:
+                        continue
+                    else:
+                        filtered_data[i] = reversed_data[i]
+                filtered_list.remove(reversed_data)
     return filtered_list
 
 
@@ -26,9 +38,9 @@ def sub_phones():
     PHONE_PATTERN = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
     PHONE_SUB = r'+7(\2)-\3-\4-\5 \6\7'
     result = []
-    for i in create_filtered_list():
-        final_names = [i[0], i[1], i[2], re.sub(PHONE_PATTERN, PHONE_SUB, i[3])]
-        result.append(final_names)
+    for i in merge_same_data():
+        final_data = [i[0], i[1], i[2], i[3], i[4], re.sub(PHONE_PATTERN, PHONE_SUB, i[5]), i[6]]
+        result.append(final_data)
     return result
 
 
